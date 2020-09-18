8980a2f6d7bc00adce4dc4493acce5b5297b607e
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-18 23:21:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,15 +3,3 @@ def find_min_in_nested_arrays(src)
 results_array = [] 
 
 
-while count < find_min_in_nested_arrays.length do
- 
-  inner_count = 0
-  while inner_count < find_min_in_nested_arrays(src)[count].length do
-    results_array << find_min_in_nested_arrays(src)[count][inner_count] 
-    inner_count += 1
-  end
- 
-  count += 1
-end
- 
-end 
